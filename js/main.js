9cf04848d1a44b02643c50d1f992e78d4b1147ad
Reuben Burda <rburda@zappos.com>
2013-04-08 23:32:04 -0700

    add alert

--- js/main.js
+++ js/main.js
@@ -13,9 +13,23 @@ var app = {
         });
     },
 
-    initialize: function() {
-        this.store = new LocalStorageStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    showAlert: function (message, title) {
+    	if (navigator.notification) {
+    		navigator.notification.alert(message, null, title, 'OK');
+    	} 
+    	else {
+    		alert(title ? (title + ": " + message) : message);
+    	}
+    },	
+    
+    initialize: 
+    	function() {
+    		var self = this;
+        	this.store = new LocalStorageStore(
+        		function(){
+        			self.showAlert('Store Initialized', 'Info');
+        		});	
+        	$('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
